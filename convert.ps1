934a6b854f766d6ed877d4cfb0a08aba1859ac66
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-11-19 14:50:30 +0900

    pauseを削除

---
+++ convert.ps1
@@ -0,0 +1,6 @@
+$files = Get-Item SourceTexture/*.png
+
+foreach($f in $files) {
+    Start-Process -FilePath x64/Debug/TextureConverter.exe -ArgumentList $f -Wait
+}
+pause
--- main.cpp
+++ main.cpp
@@ -26,6 +26,5 @@ int main(int argc, char* argv[]) {
 	// COMライブラリの終了
 	CoUninitialize();
 
-	system("pause");
 	return 0;
 }
