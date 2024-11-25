395524bc020b46173c1c7cde9535abe69bc6f022
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-11-25 14:33:51 +0900

    psの処理中に進捗を表示するように

--- convert.ps1
+++ convert.ps1
@@ -1,6 +1,8 @@
 $files = Get-Item SourceTexture/*.png
 
 foreach($f in $files) {
+    -join ("Convert Start ... ", $f)
     Start-Process -FilePath x64/Debug/TextureConverter.exe -ArgumentList $f,"-ml","3" -Wait
 }
-pause
+
+Write-Output -InputObject "End ALL Process"
